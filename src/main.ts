f715cce9d85eea4be8090b891b5639fbb8d00608
faq <facundomajda13@gmail.com>
2024-07-17 01:43:31 -0300

    Add typeorm-typedi-extensions dependency

--- src/main.ts
+++ src/main.ts
@@ -3,56 +3,43 @@ import express, { NextFunction, Request, Response } from "express";
 import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
-import { useContainer, useExpressServer } from "routing-controllers";
-import { Container } from "typedi";
-import { config } from "./common/env/environment";
-import { PaginationInterceptor } from "./common/pagination/interceptors/pagination-interceptor";
-import { AppDataSource } from "./config/database/data-source";
-import { LoggerMiddleware } from "./middlewares/logger-middleware";
+import {
+  useContainer as routingUseContainer,
+  useExpressServer,
+} from "routing-controllers";
+import { useContainer as ormUseContainer } from "typeorm";
 
-async function boostrap() {
+import { config } from "./common/env/environment";
+
+//  Config container de typeorm
+import { Container } from "typeorm-typedi-extensions";
+//       ▲ Notice how we import container from this library, instead of TypeDI.
+// import { Container } from "typedi";
+
+async function boostrap(): //  Promise<express.Application>
+Promise<express.Express> {
   const app = express();
 
   const PORT = config.server.port;
   const HOST = config.server.host;
 
-  // Configurar TypeDI para routing-controllers
-  useContainer(Container);
+  const middlewaresPath = path.join(__dirname + "/middlewares/*{.ts,.js}");
+  const controllersPath = path.join(__dirname + "/controllers/*{.ts,.js}");
+  const interceptorsPath = path.join(__dirname + "/interceptors/*{.ts,.js}");
 
-  // DataSource
-  // async function initDb() {
-  //   try {
-  //     await AppDataSource.initialize();
-  //     console.log("Database inicializada");
-  //   } catch (error) {
-  //     console.error("Falló inicializar database:", error);
-  //   }
-  // }
+  // Configurar TypeDI para routing-controllers
+  routingUseContainer(Container);
+  //   ▲ vanilla typedi
+
+  // Configurar typeorm-typedi-extensions para routing-controllers
+  /** Tell TypeORM to use the container provided by this lib to resolve it's dependencies. */
+  ormUseContainer(Container);
 
   useExpressServer(app, {
-    defaults: {
-      //with this option, null will return 404 by default
-      nullResultCode: 404,
-
-      //with this option, void or Promise<void> will return 204 by default
-      undefinedResultCode: 204,
-
-      paramOptions: {
-        //with this option, argument wont be required by default
-        required: false,
-      },
-    },
     routePrefix: "/api",
-    // controllers: [UserController],
-    controllers: [path.join(__dirname + "/controllers/*{.ts,.js}")],
-    middlewares: [
-      express.json(),
-      express.urlencoded({ extended: true }),
-      morgan("dev"),
-      helmet(),
-      LoggerMiddleware,
-    ],
-    interceptors: [PaginationInterceptor],
+    controllers: [controllersPath],
+    middlewares: [middlewaresPath],
+    interceptors: [interceptorsPath],
     validation: false,
     cors: true,
     classTransformer: false,
@@ -61,17 +48,23 @@ async function boostrap() {
       exposeUnsetFields: true,
     },
     // defaultErrorHandler: false,
-  }).listen(config.server.port, async () => {
-    // await initDb();
-    console.log(`Servidor ejecutandose en ${HOST}:${PORT}`);
+  }).listen(PORT, async () => {
+    console.log(`El servidor se está ejecutando en ${HOST}:${PORT}`);
   });
 
-  // Error handling
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(morgan("dev"));
+  app.use(helmet());
+
+  // Manejo de errores
   app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     console.error(err.stack);
     if (!res.headersSent) {
-      res.status(500).send("Algo está roto!");
+      res.status(500).send("¡Algo está roto!");
     }
   });
+
+  return app;
 }
 boostrap();
